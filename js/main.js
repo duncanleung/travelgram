993aa73d2a54355acef7e1e5b5a0d0ab49c5103a
duncanleung <leungd@gmail.com>
2015-11-30 15:08:06 -0800

    (#3) Implement JS: Hide Main Content, Show Map and Listings - onClick Discovery Tile - Implemented test on Paris Tile

--- js/main.js
+++ js/main.js
@@ -13,3 +13,35 @@ $(function() {
     }
   });
 });
+
+
+
+/*
+Discovery Tiles Section
+ ========================== */
+
+function showListings() {
+  var listingsEle = document.getElementById('listings');
+
+  listingsEle.classList.remove('hide');
+}
+
+function hideMain() {
+  var headerEle = document.querySelector('header');
+  var searchBarEle = document.getElementById('search-container');
+  var discoveryEle = document.getElementById('explore');
+  var aboutEle  = document.getElementById('about');
+  var footerEle = document.querySelector('footer');
+
+  headerEle.setAttribute('class', 'hide');
+  searchBarEle.setAttribute('class', 'hide');
+  discoveryEle.setAttribute('class', 'hide');
+  aboutEle.setAttribute('class', 'hide');
+  footerEle.setAttribute('class', 'hide');
+
+  showListings();
+}
+
+
+var parisEle = document.getElementById('paris-tile');
+parisEle.addEventListener('click', hideMain, false);
