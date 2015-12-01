e39bbbab5ba7cf69142f8c8cf838349e10ed0a46
duncanleung <leungd@gmail.com>
2015-11-30 21:10:22 -0800

    (#5) Implement 'Home' Navigation from Top Menu of Listings View

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-/*Scroll Nav Links*/
+/*
+Scroll Nav Links
+========================== */
 $(function() {
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -18,7 +20,7 @@ $(function() {
 
 /*
 Discovery Tiles Section
- ========================== */
+========================== */
 
 /* Show Section Map and Instagram Photos */
 function showListings() {
@@ -35,11 +37,11 @@ function hideMain() {
   var aboutEle  = document.getElementById('about');
   var footerEle = document.querySelector('footer');
 
-  headerEle.setAttribute('class', 'hide');
-  searchBarEle.setAttribute('class', 'hide');
-  discoveryEle.setAttribute('class', 'hide');
-  aboutEle.setAttribute('class', 'hide');
-  footerEle.setAttribute('class', 'hide');
+  headerEle.classList.add('hide');
+  searchBarEle.classList.add('hide');
+  discoveryEle.classList.add('hide');
+  aboutEle.classList.add('hide');
+  footerEle.classList.add('hide');
 
   // showListings Function Run Here
   showListings();
@@ -54,3 +56,38 @@ var tileEle = document.getElementById('discovery-tiles').querySelectorAll('a');
   e.addEventListener('click', hideMain, false);
 });
 
+
+
+/*
+Listings Nav Bar
+========================== */
+
+/* Hide Section Map and Instagram Photos */
+function hideListings() {
+  var listingsEle = document.getElementById('listings');
+
+  listingsEle.classList.add('hide');
+}
+
+/* Show All Main Content */
+function showMain() {
+  var headerEle = document.querySelector('header');
+  var searchBarEle = document.getElementById('search-container');
+  var discoveryEle = document.getElementById('explore');
+  var aboutEle  = document.getElementById('about');
+  var footerEle = document.querySelector('footer');
+
+  headerEle.classList.remove('hide');
+  searchBarEle.classList.remove('hide');
+  discoveryEle.classList.remove('hide');
+  aboutEle.classList.remove('hide');
+  footerEle.classList.remove('hide');
+
+  // hideListings Function Run Here
+  hideListings();
+}
+
+
+var logoEle = document.getElementById('home-logo');
+
+logoEle.addEventListener('click', showMain, false);
